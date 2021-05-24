ad7a77beb137e44c0a99c0e1d2c8ed4ad1555565
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2021-05-24 21:52:30 +0200

    .

---
+++ eslint/src/error.js
@@ -0,0 +1,200 @@
+console.log({ set x(x) {} });
+console.log([1,
+])
+
+console.log([
+    1])
+console.log([ 1])
+console.log([1 ])
+console.log([1,2].forEach(x=>x))
+console.log([1,2].map(x=>{console.log(x)}))
+console.log([
+    1,
+    2,3
+])
+console.log(x=>{return x})
+{var x=5}
+console.log(x)
+const aX_b = 8;
+// uncapitalized comment
+console.log(aX_b);
+class Foo {
+    ishoudlbestatic(){return 5}
+    anotherone(){}
+}
+const u=7
+,v=5
+function foo() {
+const myarray=[];
+let xx=0;
+if ((myarray[xx++] = 1)) {
+  if ((myarray[xx++] = 1)) {
+    if ((myarray[xx++] = 1)) {
+      if ((myarray[xx++] = 1)) {
+        if ((myarray[xx++] = 1)) {
+          if ((myarray[xx++] = 1)) {
+            if ((myarray[xx++] = 1)) {
+              if ((myarray[xx++] = 1)) {
+                if ((myarray[xx++] = 1)) {
+                  if ((myarray[xx++] = 1)) {
+                    if ((myarray[xx++] = 1)) {
+                      if ((myarray[xx++] = 1)) {
+                        if ((myarray[xx++] = 1)) {
+                          if ((myarray[xx++] = 1)) {
+                            if ((myarray[xx++] = 1)) {
+                              if ((myarray[xx++] = 1)) {
+                                if ((myarray[xx++] = 1)) {
+                                  if ((myarray[xx++] = 1)) {
+                                    if ((myarray[xx++] = 1)) {
+                                      if ((myarray[xx++] = 1)) {
+                                        console.log(myarray);
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+}
+foo()
+console.log({a:7}[  'a'  ])
+const ooz = function bar() {
+    if(true) {
+        return 5
+    }
+}
+const me=this
+class Bar extends Foo {
+    constructor(){return {}}
+}
+if(true) console.log(5)
+switch(1) {
+    case 1: console.log(5)
+}
+switch(1) {
+    default: console.log("what?")
+    case 1: console.log(5)
+}
+function baz(x=7,y) {
+    console.log({x,y})
+}
+this
+.foo()
+if(x==7) {
+    console.log(7)
+}
+for(let u=0;u<10;--u) {
+    console.log (u)
+    continue
+}
+/*x*/
+// this
+//that
+console.log(function(){}())
+bar( // hehe
+    7,8)
+function * gen() {}
+console.log({get x(){},set y(v){},set x(v){}})
+for(const k in {}){console.log(k)}
+const data=5;
+const _$=7
+console.log((x=>
+    x)())
+    let ubar
+if(true /* this is a very long line of code with a lot of comments and there is no point in creating a line so long */) { console.log("hello")}
+console.log(f(function(){f(function(){f(function(){f(function(){f(function(){})})})})}))
+console.log(true?"hello":"world")
+console.log(new foo())
+console.log(a.b().c().d().e().f())
+console.log(new foo)
+alert('hello')
+console.log(new Array(1,2))
+console.log(new Promise(async ()=>{}))
+const afoo = async function(){for(x in {}) { await foo()}}
+console.log(1|2)
+console.log(arguments.caller)
+switch(1){case 1:let x=5}
+Foo=7
+if(i===-0) { console.log("")}
+if(foo=5) { console.log("")}
+foo = foo => 1 ? 2 : 3
+data=7
+const pattern1 = /\x1f/;
+debugger;
+delete foo;
+const p2=/=5/
+console.log(function(a,b,a){})
+class Baz {
+  iti(){}
+  iti(){}
+}
+let mycounter=5;
+let myothercounter=7;
+if(mycounter++===--myothercounter){console.log("A")}else if(mycounter++===--myothercounter) {console.log("B")} else if(mycounter++===--myothercounter){console.log("C")}
+console.log({x:1,x:2})
+switch(1){case 1:case 1:break}
+console.log(function(){if(true){return 5}else{return 6}})
+const p3=/[]/
+if(foo==null){}
+const {}={}
+eval("")
+try{}catch(e){e=5}
+Object.prototype.extra=5
+console.log((()=>{}).bind(this))
+console.log((function(){}).bind({}))
+if(!!true){}
+x:while(false){break x};
+console.log(5.)
+window={}
+setTimeout("",1000)
+if(true) {
+  function bibam(){}
+}
+console.log(new RegExp("["))
+console.log(function(){
+  "use strict";
+  return this
+})
+Foo.prototype.__iterator__=5
+if(true){}else{if(true){}}
+for(x in foo) {foo.x= ()=>x}
+const mynumber = 123456789123456798123456798
+const pipi=/[👍]/
+
+
+
+if(true||true&&true){} 
+const titi=tata=5
+console.log("aaa\
+bbb")
+if(!true)
+baz()
+else
+baz()
+console.log(true?false?1:2:3)
+new Foo()
+console.log(new Function())
+console.log(new Object())
+console.log(new Symbol())
+console.log(new String())
+console.log("\8")
+console.log(Math())
+console.log("\5")
+console.log(0555)
+console.log(function(x){x=5})
+console.log(new Promise(()=>{return 5}))
+console.log({}.__proto__)
+console.log({}.hasOwnProperty(''))
