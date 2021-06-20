481e4c4285953896409107129f631bb21f16d09f
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2021-06-20 14:43:09 +0200

    .

--- welcome/numbers/src/test/java/numbers/DangerousImplicitCastTest.java
+++ welcome/numbers/src/test/java/numbers/DangerousImplicitCastTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-class DangerousImplicitCast {
+class DangerousImplicitCastTest {
 
     @Test
     void testLongToFloatAndBack() {
